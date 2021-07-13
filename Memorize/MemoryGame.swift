823b522f641b904b4708874db2437d10780d8ec9
Antonio Bunicic <antonio.bunicic@gmail.com>
2021-07-13 10:54:59 +0200

    Make index method return Optional<Int>

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -11,17 +11,19 @@ struct MemoryGame<CardContent> {
     private(set) var cards: Array<Card>
     
     mutating func choose(_ card: Card) {
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+        if let chosenIndex = index(of: card) {
+            cards[chosenIndex].isFaceUp.toggle()
+        }
         print("\(cards)")
     }
-    func index(of card: Card) -> Int {
+    
+    func index(of card: Card) -> Int? {
         for index in 0..<cards.count {
             if cards[index].id == card.id {
                 return index
             }
         }
-        return 0 // bogus
+        return nil
     }
     
     init(numberOfPairs: Int, createCardContent: (Int) -> CardContent) {
